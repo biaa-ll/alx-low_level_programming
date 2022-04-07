e318eaf1ff674249ae3d564d81cd6da2cb27a824
olbiaaaa <olbiaallll@gmail.com>
2022-04-07 11:52:09 -0700

    task six

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+
+/**
+/**                                                                                                                                   
+
+                                                                                                                                      
+
+* main - Entry point                                                                                                                  
+
+                                                                                                                                      
+
+*                                                                                                                                     
+
+                                                                                                                                      
+
+* REturn Always 0 (Success)                                                                                                           
+
+                                                                                                                                      
+
+*/                                                                                                                                    
+
+                                                                                                                                      
+
+int main(void)                                                                                                                        
+
+                                                                                                                                      
+
+{                                                                                                                                     
+
+                                                                                                                                      
+
+int a;                                                                                                                                
+
+                                                                                                                                      
+
+long int b;                                                                                                                           
+
+                                                                                                                                      
+
+long long int c;                                                                                                                      
+
+                                                                                                                                      
+
+char d;                                                                                                                               
+
+                                                                                                                                      
+
+float f;                                                                                                                              
+
+                                                                                                                                      
+
+printf("Size of a char: %lu byte(s)\n", (unsigned long)size of(d));                                                                   
+
+                                                                                                                                      
+
+printf("Size of an int: %lu bytes(s)\n", (unsigned long)size of(a));                                                                  
+
+                                                                                                                                      
+
+printf("Size of a long int: %lu bytes(s)\n", (unsigned long)size of(                                                                  
+
+b));                                                                                                                                  
+
+printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)siz                                                                  
+
+e of(c));                                                                                                                             
+
+printf("Size of a float: %lu byte(s)\n", (unsigned long)size of(f));                                                                  
+
+return (0);                                                                                                                           
+
+}                                                                                                                                     
+
+     
