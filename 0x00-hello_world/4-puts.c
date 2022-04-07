04022dd68e0310ce3c79c91b3a9c3ca51acaf202
olbiaaaa <olbiaallll@gmail.com>
2022-04-07 12:22:00 -0700

    task for

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,37 @@
-#include <stdio.h>
+#include <stdio.h>                                                  
 
+                                                                    
 
+                                                                    
 
-/**
+                                                                    
 
- * main - Entry point
+/**                                                                 
 
- *
+                                                                    
 
- * Return: Always 0 (Success)
+ * main - Entry point                                               
 
- */
+                                                                    
 
-int main(void)
+ *                                                                  
 
-{
-puts("\"Programming is like building a multilingual puzzle\n");
+                                                                    
+
+ * Return: Always 0 (Success)                                       
+
+                                                                    
+
+ */                                                                 
+
+                                                                    
+
+int main(void)                                                      
+
+                                                                    
+
+{                                                                   
+
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
