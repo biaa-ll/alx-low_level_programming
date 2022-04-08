a2fbc6c1453c17861d0f273eca0bdc32381f129e
olbiaaaa <olbiaallll@gmail.com>
2022-04-08 13:13:51 -0700

    nine

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,17 +9,16 @@
 int main(void)
 {
 int num;
-for (num = 48; num <= 57; num ++)
+for (num = 48; num <= 57; num++)
 {
 putchar(num);
 if (num == 57)
 {
 break;
 }
-putchar ( ',');
+putchar (',');
 putchar (' ');
 }
 putchar('\n');
 return (0);
 }
-	   
