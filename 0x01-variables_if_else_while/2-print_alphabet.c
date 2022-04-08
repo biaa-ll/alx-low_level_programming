efaeb4a5ebc5ffc3a5081a51d25c1feed2181be6
olbiaaaa <olbiaallll@gmail.com>
2022-04-08 11:31:39 -0700

    task 3

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,17 +8,22 @@
  *
  */
 int main(void)
+
 {
 char lower;
 char upper;
+
 for (lower = 'a'; lower <= 'z'; lower++)
 {
 putchar(lower);
 }
+ 
 for (upper = 'A'; upper <= 'Z'; upper++)
 {
 putchar(upper);
 }
-putchar  ('\n');
+
+putchar("\n");
+ 
 return (0);
 }
