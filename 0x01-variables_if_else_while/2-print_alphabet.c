b5f71c7487450a168982a28cb723daa54b678a58
olbiaaaa <olbiaallll@gmail.com>
2022-04-08 10:47:32 -0700

    task two

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success/correct)
+ *
+ */
+int main(void)
+{
+char alphabet;
+
+for (aplabet = 'a'; alphabets <= 'z'; alphabet++)
+{
+putchar(alphabet);
+}
+putchar('\n');
+return (0);
+}
