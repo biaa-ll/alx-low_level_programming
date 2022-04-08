451e7507d5939e268be4bde047a0c4216f21a19f
olbiaaaa <olbiaallll@gmail.com>
2022-04-08 11:47:57 -0700

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,25 +1,57 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include <stdlib.h>                                                                                                                   
 
-/**
- * main - Entry point
- *Return: Always 0 (Success/correct)
- *
- */
-int main(void)
-{
-char lower;
-char upper;
-for (upper = 'A'; upper <= 'Z'; upper++)
-{
-putchar(upper);
+#include <time.h>                                                                                                                     
+
+#include <stdio.h>                                                                                                                    
+
+                                                                                                                                      
+
+/**                                                                                                                                   
+
+ * main - Entry point                                                                                                                 
+
+ * Return: Always 0 (Success/correct)                                                                                                 
+
+ *                                                                                                                                    
+
+ */                                                                                                                                   
+
+int main(void)                                                                                                                        
+
+                                                                                                                                      
+
+{                                                                                                                                     
+
+char lower;                                                                                                                           
+
+char upper;                                                                                                                           
+
+                                                                                                                                      
+
+for (lower = 'a'; lower <= 'z'; lower++)                                                                                              
+
+{                                                                                                                                     
+
+putchar(lower);                                                                                                                       
+
+}                                                                                                                                     
+
+                                                                                                                                      
+
+for (upper = 'A'; upper <= 'Z'; upper++)                                                                                              
+
+{                                                                                                                                     
+
+putchar(upper);                                                                                                                       
+
+}                                                                                                                                     
+
+                                                                                                                                      
+
+putchar("\n");                                                                                                                        
+
+                                                                                                                                      
+
+return (0);                                                                                                                           
+
 }
-for (lower = 'a'; lower <= 'z'; lower++)
-{
-putchar(lower);
-}
-putchar('\n');
-return(0);
-}
-  
