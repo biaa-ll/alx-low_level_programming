63b27aed0a76bf31133541cfa1f112b1d1d6174a
olbiaaaa <olbiaallll@gmail.com>
2022-04-08 11:56:46 -0700

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Entry point 
- * 
+ * main - Entry point
+ *
  * Return: Always 0 (Success/correct)
  */
 
 int main(void)
 {
-  char alpha;
+char alpha;
 
 for (alpha = 'a'; alpha <= 'z'; alpha++)
 {
