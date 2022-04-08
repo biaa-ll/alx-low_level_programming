0b2234f5b5e36ebf3f074041288be5881ba3bc9e
olbiaaaa <olbiaallll@gmail.com>
2022-04-08 11:07:20 -0700

    task two

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -15,6 +15,10 @@ for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
 putchar(alphabet);
 }
+for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+{
+putchar(alphabet);
+}
 putchar('\n');
 return (0);
 }
