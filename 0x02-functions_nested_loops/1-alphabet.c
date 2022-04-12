9ef20e45e0de9ddb97440f1525ae86e92fd7b001
olbiaaaa <olbiaallll@gmail.com>
2022-04-12 09:19:32 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,11 +7,11 @@
  * Return: Always 0.
  */
 void print_alphabet(void)
-{  
+{
 char alphabet;
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-{      
-_putchar(alphabet);      
-}  
-_putchar('\n'); 
+{
+_putchar(alphabet);
+}
+_putchar('\n');
 }
