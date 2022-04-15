c6b641883969ef4c5164859d53fbb6f4c8a10db0
olbiaaaa <olbiaallll@gmail.com>
2022-04-15 01:11:39 -0700

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,7 +5,7 @@
  * @letter: the character to be tracked
  * Return: 1 if c is uppercase, 0 otherwise
  */
-int _isupper(int letter)  
+int _isupper(int letter)
 {
 return (letter >= 'A' && letter <= 'Z');
 }
